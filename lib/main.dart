8f05178df94592fafa8c2d95eaa67266cbb58e5c
Elsa-2002 <elsaeldhose2002@gmail.com>
2021-09-21 15:41:50 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,86 @@ class _MyHomePageState extends State<MyHomePage> {
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
+      body: Container(
+          height: 700,
+          width: double.infinity,
+          padding: EdgeInsets.all(30),
+          child: Column(
+            children: [
+              Align(child: Text("PORTFOLIO"), alignment: Alignment.center),
+              SizedBox(height: 30),
+              CircleAvatar(
+                backgroundColor: Colors.brown,
+                child: const Text('Divya'),
+                radius: 70,
+              ),
+              Spacer(),
+              Row(
+                children: [
+                  Text("Name"),
+                  Spacer(),
+                  Text("Divya Roy"),
+                ],
+              ),
+              Spacer(),
+              Divider(
+                color: Colors.black12,
+                thickness: 1,
+              ),
+              Spacer(),
+              Row(
+                children: [
+                  Text("Email"),
+                  Spacer(),
+                  Text("Divya123@gmail.com"),
+                ],
+              ),
+              Spacer(),
+              Divider(
+                color: Colors.black12,
+                thickness: 1,
+              ),
+              Spacer(),
+              Row(
+                children: [
+                  Text("Phone"),
+                  Spacer(),
+                  Text("9789098760"),
+                ],
+              ),
+              Spacer(),
+              Divider(
+                color: Colors.black12,
+                thickness: 1,
+              ),
+              Spacer(),
+              Row(
+                children: [
+                  Text("Date Of Birth"),
+                  Spacer(),
+                  Text("02/12/1994"),
+                ],
+              ),
+              Spacer(),
+              Divider(
+                color: Colors.black12,
+                thickness: 1,
+              ),
+              Spacer(),
+              Row(
+                children: [
+                  Text("Address"),
+                  Spacer(),
+                  Text("Central Street,Delhi"),
+                ],
+              ),
+              Spacer(),
+              Divider(
+                color: Colors.black12,
+                thickness: 1,
+              ),
+            ],
+          )),
     );
   }
 }
